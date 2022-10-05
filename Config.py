a91b579f9dedc3cbebda80fe7398deaa5b9fb701
Mohamed Rizad <rizadvillan@gmail.com>
2022-10-05 18:10:37 +0530

    Variable's!!!!!!

---
+++ Config.py
@@ -0,0 +1,8 @@
+API_KEY='d79fe6458d9a29da04d15e3e' #if you have API replace this with your API KEY
+API_ID = '<PUT API-ID HERE>' #replace this with API ID
+API_HASH = '<PUT API-HASH HERE>' #replace this with API HASH
+BOT_TOKEN = '<PUT BOT-TOKEN HERE>' #replace this with BOT-TOKEN
+
+
+base_url='https://v6.exchangerate-api.com/v6/'+API_KEY+'/latest/'
+base_currency='USD'
